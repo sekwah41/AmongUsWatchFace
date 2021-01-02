a8dffb2c43b72b9eea0d1290ecaae23c8b9809e8
Sekwah <sekwah41@gmail.com>
2021-01-02 20:41:03 +0000

    Base watch face

--- app/index.js
+++ app/index.js
@@ -1,6 +1,3 @@
-/*
- * Entry point for the watch app
- */
 import clock from "clock";
 import { FitFont } from 'fitfont'
 
@@ -13,12 +10,25 @@ const timeFont = new FitFont({
     letterspacing: 0
 });
 
-timeFont.text = '12:55';
+
+const timeAMPM = new FitFont({
+    id:'amongUsAMPM',
+    font:'Amatic_SC_40',
+
+    halign: 'middle',
+    valign: 'middle',
+    letterspacing: 0
+});
+
+timeFont.text = '00:00';
 
 clock.granularity = "minutes"; // seconds, minutes, or hours
 
 clock.addEventListener("tick", (evt) => {
     let hours = evt.date.getHours();
-    if (hours > 12) hours -= 12;
+    if (hours > 12) {
+        hours -= 12;
+    }
     timeFont.text = `${hours}:${evt.date.getMinutes()}`;
+    timeAMPM.text = 'AM';
 })
